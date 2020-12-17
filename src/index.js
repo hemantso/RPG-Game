d89e5b7b2c236f88adb5ff98d9b738bf4c95f04f
hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2020-12-18 00:56:06 +0530

    gamescene added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import 'phaser';
-import SampleScene from './scenes/sample-scene';
+import Phaser from 'phaser';
 
-const gameConfig = {
-  width: 680,
-  height: 400,
-  scene: SampleScene
-};
-new Phaser.Game(gameConfig);
+import config from './config/config';
+import GameScene from './scenes/gameScene';
+
+class Game extends Phaser.Game {
+    constructor() {
+      super(config);
+      this.scene.add('Game', GameScene);
+      this.scene.start('Boot');
+    }
+  }
+  window.game = new Game();
