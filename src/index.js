6ec72adf0785d9a92142ff689ab28f114c1a6ee6
hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2020-12-18 21:24:54 +0530

    bootscene was added

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import Phaser from 'phaser';
 
 import config from './config/config';
 import GameScene from './scenes/gameScene';
+import BootScene from './scenes/bootScene';
 
 class Game extends Phaser.Game {
     constructor() {
       super(config);
+      this.scene.add('Boot', BootScene);
       this.scene.add('Game', GameScene);
       this.scene.start('Boot');
     }
